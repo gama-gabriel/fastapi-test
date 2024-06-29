4630f2bbd0c050717b8a576b383d13e7553d6242
Gabriel <bielmgama@gmail.com>
2024-06-29 20:25:25 -0300

    changed cron task method

--- index.py
+++ index.py
@@ -5,19 +5,21 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from apscheduler.schedulers.background import BackgroundScheduler
 from contextlib import asynccontextmanager
+from fastapi_utilities import repeat_at
 import datetime
 
 def check_time():
     print(datetime.datetime.now())
 
-@asynccontextmanager
-async def lifespan(app:FastAPI):
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(check_time,"cron", second = '*/5')
-    scheduler.start()
-    yield
+# @asynccontextmanager
+# async def lifespan(app:FastAPI):
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(check_time,"cron", second = '*/5')
+#     scheduler.start()
+#     yield
 
-app = FastAPI(lifespan=lifespan)
+# app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -25,6 +27,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )   
+
+@app.on_event('startup')
+@repeat_at(cron='* * * * *')
+async def hi():
+    print(datetime.datetime.now())
 
 @app.get('/epa')
 async def read_root():
