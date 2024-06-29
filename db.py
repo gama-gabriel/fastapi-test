65df0d0bdeab681753c7e6a37056bec26cb72014
Gabriel <bielmgama@gmail.com>
2024-06-29 18:25:04 -0300

    initial commit

---
+++ db.py
@@ -0,0 +1,37 @@
+import time
+import polars as pl
+
+def get_epa(down=[1,2,3,4]):
+    start = time.perf_counter()
+
+    d = (
+    pl.scan_parquet('desc.parquet')
+    .select(pl.col('team_abbr').alias('Team'), pl.col('team_name'), pl.col('team_color'), pl.col('team_logo_espn'))
+    )
+    desc = d.collect()
+
+    q = (
+    pl.scan_parquet('raw.parquet')
+    .filter(((pl.col('pass') == 1) & (pl.col('week') <= 17)) )
+    .group_by(pl.col('posteam').alias("Team"))
+    .agg(pl.col('epa').mean().alias("Offensive EPA"))
+    .sort('Offensive EPA', descending = True)
+    )
+    df = q.collect()
+
+    epa = df.join(desc, on="Team", how="inner")
+
+
+    offensive_epa = epa['Offensive EPA'].to_list()
+    teams = epa['Team'].to_list()
+    logos = epa['team_logo_espn'].to_list()
+    colors = epa['team_color'].to_list()
+
+    data_list = [{'data': {'x': x}, 'name': name, 'logo': logo, 'color': color} 
+		 for x, name, logo, color in zip(offensive_epa, teams, logos, colors)]
+
+    epa_json = str(data_list)
+
+    end = time.perf_counter()
+    print(end - start)
+    return epa_json
---
+++ index.py
@@ -0,0 +1,33 @@
+from db import get_epa
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+from apscheduler.schedulers.background import BackgroundScheduler
+from contextlib import asynccontextmanager
+import datetime
+
+def check_time():
+    print(datetime.datetime.now())
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(check_time,"cron", second = '*/5')
+    scheduler.start()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)   
+
+@app.get('/')
+async def read_root():
+    return JSONResponse(content=jsonable_encoder(get_epa()))
+
+
