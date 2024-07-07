ebd08733eac32900eababd9f01c7ce92ef966b33
Gabriel <bielmgama@gmail.com>
2024-07-07 03:06:56 -0300

    added joins to emulate load functions

--- api/load.py
+++ api/load.py
@@ -1,4 +1,4 @@
-from polars import read_parquet as pl_read_parquet
+import polars as pl
 from fsspec import open as fs_open
 from requests import get
 from time import perf_counter
@@ -8,9 +8,18 @@ def requests_opt():
 
     start = perf_counter()
 
-    pbp = get(url)
-    pbp.raise_for_status()
-    pbp = pl_read_parquet(pbp.content)
+    raw = get(url)
+    raw.raise_for_status()
+    raw = pl.read_parquet(raw.content)
+    raw = raw.cast({'play_id': pl.Int32})
+
+    url = 'https://github.com/nflverse/nflverse-data/releases/download/pbp_participation/pbp_participation_2023.parquet'
+
+    part = get(url)
+    part.raise_for_status()
+    part = pl.read_parquet(part.content)
+
+    pbp = raw.join(part, on=['old_game_id', 'play_id'], how='left')
     
     print(pbp.shape)
     end = perf_counter()
@@ -22,8 +31,16 @@ def fsspec_opt():
     start = perf_counter()
 
     with fs_open(url) as file:
-        pbp = pl_read_parquet(file)
+        raw = pl.read_parquet(file)
+        raw = raw.cast({'play_id': pl.Int32})
+
+    url = 'https://github.com/nflverse/nflverse-data/releases/download/pbp_participation/pbp_participation_2023.parquet'
+
+    with fs_open(url) as file:
+        part = pl.read_parquet(file)
+
+    pbp = raw.join(part, on=['old_game_id', 'play_id'], how='left')
     
-    end = perf_counter()
     print(pbp.shape)
+    end = perf_counter()
     return (end - start)
