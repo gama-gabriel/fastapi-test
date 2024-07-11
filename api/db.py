d3c0b7d593043de7c9722ac8870d078a075bfecc
Gabriel <bielmgama@gmail.com>
2024-07-10 21:28:18 -0300

    removed load funcitons and refactored get_epa fn

--- api/db.py
+++ api/db.py
@@ -4,23 +4,22 @@ import polars as pl
 def get_epa(year: int, down=[1,2,3,4]):
     start = time.perf_counter()
 
-    d = (
-    pl.scan_parquet('api/desc.parquet')
-    .select(pl.col('team_abbr').alias('Team'), pl.col('team_name'), pl.col('team_color'), pl.col('team_logo_espn'))
+    desc = (
+        pl.scan_parquet('api/desc.parquet')
+        .select(pl.col('team_abbr').alias('Team'), pl.col('team_name'), pl.col('team_color'), pl.col('team_logo_espn'))
     )
-    desc = d.collect()
 
-    q = (
-    pl.scan_parquet(f'/tmp/pbp_{year}.parquet')
-    .filter(((pl.col('pass') == 1) & (pl.col('week') <= 18)) )
-    .group_by(pl.col('posteam').alias("Team"))
-    .agg(pl.col('epa').mean().alias("Offensive EPA"))
-    .sort('Offensive EPA', descending = True)
+    pbp = (
+        pl.scan_parquet(f'/tmp/pbp_{year}.parquet')
+        .filter(((pl.col('pass') == 1) & (pl.col('week') <= 18)) )
+        .group_by(pl.col('posteam').alias("Team"))
+        .agg(pl.col('epa').mean().alias("Offensive EPA"))
+        .sort('Offensive EPA', descending = True)
     )
-    df = q.collect()
 
-    epa = df.join(desc, on="Team", how="inner")
+    df = pbp.join(desc, on="Team", how="inner")
 
+    epa = df.collect()
 
     offensive_epa = epa['Offensive EPA'].to_list()
     teams = epa['Team'].to_list()
