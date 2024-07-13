0e119e3bdad016e06c2db17439c47d9d3d5bfaa7
Gabriel <bielmgama@gmail.com>
2024-07-12 23:38:11 -0300

    fixed typo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo oi 
 python3.12 -m venv env 
 source env/bin/activate 
-pip3.12 install -r --disable-pip-version-check --no-chache-dir requirements.txt
+pip3.12 install -r --disable-pip-version-check --no-cache-dir requirements.txt
